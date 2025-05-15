9a71ae0a4e17cba9142dc9cfc37caf31584d2d3a
tarun-wegile <31913931+tarun-wegile@users.noreply.github.com>
2025-05-15 18:15:19 +0530

    Android removeView crash solved
    Android removeView crash solved

--- android/src/main/kotlin/com/example/rppg_common/FLNativeView.kt
+++ android/src/main/kotlin/com/example/rppg_common/FLNativeView.kt
@@ -1,4 +1,4 @@
-package com.example.rppg_common
+/*package com.example.rppg_common
 
 import android.content.Context
 import android.view.View
@@ -24,5 +24,49 @@ internal class FLNativeView(context: Context, id: Int,creationParams: Map<String
         Analysis.getInstance().rppgCameraView.layoutParams = lps
         frameLayout.addView(Analysis.getInstance().rppgCameraView)
         view = frameLayout
+    }
+} */
+
+package com.example.rppg_common
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import io.flutter.plugin.platform.PlatformView
+
+internal class FLNativeView(
+    context: Context,
+    id: Int,
+    creationParams: Map<String?, Any?>?
+) : PlatformView {
+
+    private val frameLayout: FrameLayout
+    private val rppgCameraView: View = Analysis.getInstance().rppgCameraView
+
+    override fun getView(): View {
+        return frameLayout
+    }
+
+    override fun dispose() {
+        (rppgCameraView.parent as? ViewGroup)?.removeView(rppgCameraView)
+    }
+
+    init {
+        frameLayout = FrameLayout(context).apply {
+            layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.MATCH_PARENT
+            )
+
+            (rppgCameraView.parent as? ViewGroup)?.removeView(rppgCameraView)
+
+            rppgCameraView.layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.MATCH_PARENT
+            )
+
+            addView(rppgCameraView)
+        }
     }
 }
